3352185c4bec0ce7b18f152a68eddb16e2bd9a9d
Davi S. <149006867+daviloko00-00@users.noreply.github.com>
2025-08-25 15:31:00 -0300

    Feito um comentário para esclarecer o uso de um método arcaico

--- AT.2/app.js
+++ AT.2/app.js
@@ -12,8 +12,31 @@ app.get("/calculadora", (req, res) => {
         if (isNaN(numUm) || isNaN(numDois) || !operacoesValidas.includes(operacao)) {
             return res.status(400).send("ERRO 400. Requisição Mal Feita.");
         }
+        
 
         let resultado;
+
+        // sei como fazer via switch case mas preferi ser mais arcaico
+        /*switch (operacao) {
+            case "soma":
+                resultado = n1 + n2;
+                return res.send(`A soma de ${n1} + ${n2} é ${resultado}`);
+            case "subtracao":
+                resultado = n1 - n2;
+                return res.send(`A subtração de ${n1} - ${n2} é ${resultado}`);
+            case "multiplicacao":
+                resultado = n1 * n2;
+                return res.send(`A multiplicação de ${n1} * ${n2} é ${resultado}`);
+            case "divisao":
+                if (n2 === 0) {
+                    return res.status(400).send("Erro: divisão por zero não é permitida.");
+                }
+                resultado = n1 / n2;
+                return res.send(`A divisão de ${n1} / ${n2} é ${resultado}`);
+            default:
+                return res.status(400).send("Operação inválida.");
+        }*/
+        
         //soma
         if (operacao === "soma") {
             resultado = parseFloat(numUm) + parseFloat(numDois);
